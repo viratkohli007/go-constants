6ff272a9d4181eb72ac24aead2c1250169801c03
viratkohli007 <aniket.amin@bacancytechnology.com>
2019-01-01 18:12:17 +0530

    constants go

---
+++ constants.go
@@ -0,0 +1,11 @@
+package main;
+import "fmt"
+import "math"
+const a string = "optimus prime" 
+func main()  {
+	const b int = 500000000
+	const c = 320/b
+	fmt.Println(c)
+	fmt.Println(int(c))
+	fmt.Println(math.Cos(90))
+}
